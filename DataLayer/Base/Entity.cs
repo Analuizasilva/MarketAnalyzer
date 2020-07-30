b223fb3662d2fa02cc7e6887af295b54e97a6fb8
s-cunha <s.cunha@recodme.pt>
2020-07-30 15:24:51 +0100

    Base entities

---
+++ DataLayer/Base/Entity.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DataLayer.Base
+{
+    public abstract class Entity
+    {
+        [Key]
+        public Guid Id { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime UpdatedAt { get; private set; }
+
+        private bool _isDeleted;
+
+        public bool IsDeleted
+        {
+            get => _isDeleted;
+            set
+            {
+                _isDeleted = value;
+                RegisterChange();
+            }
+        }
+
+        protected void RegisterChange()
+        {
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        protected Entity() : this(Guid.NewGuid(), DateTime.UtcNow, DateTime.UtcNow, false)
+        {
+
+        }
+
+        protected Entity(Guid id, DateTime createdAt, DateTime updatedAt, bool isDeleted)
+        {
+            Id = id;
+            CreatedAt = createdAt;
+            UpdatedAt = updatedAt;
+            IsDeleted = isDeleted;
+        }
+    }
+}
--- MarketAnalyzer/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MarketAnalyzer
-{
-    public class Class1
-    {
-    }
-}
