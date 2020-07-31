41ae728c4e021068cedb47a18d1016b2769b5d4b
Analuizasilva <a.silva@recodme.pt>
2020-07-31 15:33:35 +0100

    Added Company Class in Data Layer.

--- DataLayer/Company.cs
+++ DataLayer/Company.cs
@@ -1,10 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Recodme.Labs.MarketAnalyzer.DataLayer.Base;
+using System;
+using System.ComponentModel.DataAnnotations;
 
-namespace DataLayer
+namespace Recodme.Labs.MarketAnalyzer.DataLayer
 {
-    public class Company
+    public class Company : NamedEntity
     {
+        [Required]
+        [Display(Name = "Ticker")]
+        private string _ticker;
+        public string Ticker
+        {
+            get => _ticker;
+            set
+            {
+                _ticker = value;
+                RegisterChange();
+            }
+        }
+
+        [Required]
+        [Display(Name = "Description")]
+        private string _description;
+        public string Description
+        {
+            get => _description;
+            set
+            {
+                _description = value;
+                RegisterChange();
+            }
+        }
+
+        [Required]
+        [Display(Name = "Rank")]
+        private int _rank;
+        public int Rank
+        {
+            get => _rank;
+            set
+            {
+                _rank = value;
+                RegisterChange();
+            }
+        }
+
+        public Company(string name) : base(name)
+        {
+
+        }
+
+        public Company(string name, string ticker,  string description, int rank) : base(name)
+        {
+            _ticker = ticker;
+            _description = description;
+            _rank = rank;
+        }
+
+        public Company(Guid id, DateTime createdAt, DateTime updatedAt, string name, string ticker, string description, int rank) : base(id, createdAt, updatedAt, name)
+        {
+            _ticker = ticker;
+            _description = description;
+            _rank = rank;
+        }
     }
 }
