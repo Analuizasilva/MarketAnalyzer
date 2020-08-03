bf6d3768c01aa6036f53e7dabbcb410d1fd6a647
Marco Pereira <m.pereira@recodme.pt>
2020-08-03 16:42:52 +0100

    merge

--- Recodme.Labs.MarketAnalyzer.App/Program.cs
+++ Recodme.Labs.MarketAnalyzer.App/Program.cs
@@ -11,45 +11,68 @@ namespace Recodme.Labs.MarketAnalyzer.App
     {
         static void Main(string[] args)
         {
-            var _ctx = new Context();
-            _ctx.Database.EnsureCreated();
+            //var _ctx = new Context();
+            //_ctx.Database.EnsureCreated();
+
+            //HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
+            //HtmlAgilityPack.HtmlDocument doc = web.Load("https://www.slickcharts.com/sp500");
+
+            //var headerContent = doc.DocumentNode
+            //    .SelectNodes("//div[@class='table-responsive']").Descendants("a").ToList();
+
+
+            //var companyNames = new List<HtmlAgilityPack.HtmlNode>();  //par
+            //var companyTicker = new List<HtmlAgilityPack.HtmlNode>(); //impar
+
+
+            //for (var i = 0; i < headerContent.Count; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        var data = headerContent[i];
+            //        companyNames.Add(data);
+            //    }
+            //    else if (i % 2 == 1)
+            //    {
+            //        var data = headerContent[i];
+            //        companyTicker.Add(data);
+            //    }
+            //}
+
+
+            //foreach (var item in companyTicker)
+            //{
+
+            //    Console.WriteLine(item.InnerText);
+            //}
+
+            //_ctx.Companies.AddRange();
+
+
+
 
             HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
             HtmlAgilityPack.HtmlDocument doc = web.Load("https://www.slickcharts.com/sp500");
 
             var headerContent = doc.DocumentNode
-                .SelectNodes("//div[@class='table-responsive']").Descendants("a").ToList();
+                .SelectNodes("//td[@class='text-nowrap']").ToList();
 
-
-            var companyNames = new List<HtmlAgilityPack.HtmlNode> ();  //par
-            var companyTicker = new List<HtmlAgilityPack.HtmlNode> (); //impar
-
-
-            for (var i = 0; i < headerContent.Count; i++) 
+            foreach (var item in headerContent)
             {
-                if (i % 2 == 0)
-                {
-                    var data = headerContent[i];
-                    companyNames.Add(data);
-                }
-                else if(i%2==1)
-                {
-                    var data = headerContent[i];
-                    companyTicker.Add(data);
-                }
-            }
+                    Console.WriteLine(item.InnerText);
+             }
 
 
-            foreach (var item in companyTicker)
-            {
 
-                Console.WriteLine(item.InnerText);
-            }
 
-            _ctx.Companies.AddRange();
+
+
 
 
             
+
+
+
         }
     }
 }
