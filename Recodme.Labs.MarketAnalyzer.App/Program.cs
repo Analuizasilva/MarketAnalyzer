018782e1ebca31afb51f4a0b7af4e7f4d41ba32f
Marco Pereira <m.pereira@recodme.pt>
2020-08-03 14:29:51 +0100

    Added scrap

--- Recodme.Labs.MarketAnalyzer.App/Program.cs
+++ Recodme.Labs.MarketAnalyzer.App/Program.cs
@@ -1,5 +1,6 @@
 ﻿using DataAccessLayer.Contexts;
 using System;
+using System.Linq;
 
 namespace Recodme.Labs.MarketAnalyzer.App
 {
@@ -7,8 +8,18 @@ namespace Recodme.Labs.MarketAnalyzer.App
     {
         static void Main(string[] args)
         {
-            var ctx = new Context();
-            ctx.Database.EnsureCreated();
+
+            HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
+            HtmlAgilityPack.HtmlDocument doc = web.Load("https://www.slickcharts.com/sp500");
+
+            var Name = doc.DocumentNode
+                .SelectNodes("//div[@class='table-responsive']").ToList();
+
+            foreach(var item in Name)
+            {
+                Console.WriteLine(item.InnerText);
+            }
+
         }
     }
 }
