564aefbb983e29029455ff34d3ae25b50a2180ba
s-cunha <s.cunha@recodme.pt>
2020-08-03 12:05:57 +0100

    Created App

---
+++ Recodme.Labs.MarketAnalyzer.DataLayer.App/Program.cs
@@ -0,0 +1,14 @@
+﻿using DataAccessLayer.Contexts;
+using System;
+
+namespace Recodme.Labs.MarketAnalyzer.DataLayer.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ctx = new Context();
+            ctx.Database.EnsureCreated();
+        }
+    }
+}
