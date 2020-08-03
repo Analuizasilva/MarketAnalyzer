aa96570a7a3e7c0e0d17bb5165b59a30cf011516
Marco Pereira <m.pereira@recodme.pt>
2020-08-03 15:19:30 +0100

    scrap updated

--- Recodme.Labs.MarketAnalyzer.App/Program.cs
+++ Recodme.Labs.MarketAnalyzer.App/Program.cs
@@ -1,6 +1,9 @@
 ﻿using DataAccessLayer.Contexts;
+using Recodme.Labs.MarketAnalyzer.DataLayer;
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 
 namespace Recodme.Labs.MarketAnalyzer.App
 {
@@ -8,18 +11,45 @@ namespace Recodme.Labs.MarketAnalyzer.App
     {
         static void Main(string[] args)
         {
+            var _ctx = new Context();
+            _ctx.Database.EnsureCreated();
 
             HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
             HtmlAgilityPack.HtmlDocument doc = web.Load("https://www.slickcharts.com/sp500");
 
-            var Name = doc.DocumentNode
-                .SelectNodes("//div[@class='table-responsive']").ToList();
+            var headerContent = doc.DocumentNode
+                .SelectNodes("//div[@class='table-responsive']").Descendants("a").ToList();
 
-            foreach(var item in Name)
+
+            var companyNames = new List<HtmlAgilityPack.HtmlNode> ();  //par
+            var companyTicker = new List<HtmlAgilityPack.HtmlNode> (); //impar
+
+
+            for (var i = 0; i < headerContent.Count; i++) 
             {
+                if (i % 2 == 0)
+                {
+                    var data = headerContent[i];
+                    companyNames.Add(data);
+                }
+                else if(i%2==1)
+                {
+                    var data = headerContent[i];
+                    companyTicker.Add(data);
+                }
+            }
+
+
+            foreach (var item in companyTicker)
+            {
+
                 Console.WriteLine(item.InnerText);
             }
 
+            _ctx.Companies.AddRange();
+
+
+            
         }
     }
 }
