bddf491d113985fefde540b88eff9f727a3cb45c
Analuizasilva <a.silva@recodme.pt>
2020-08-03 14:31:12 +0100

    merge

--- Recodme.Labs.MarketAnalyzer.App/Program.cs
+++ Recodme.Labs.MarketAnalyzer.App/Program.cs
@@ -1,5 +1,6 @@
 ﻿using DataAccessLayer.Contexts;
 using System;
+using System.Net;
 
 namespace Recodme.Labs.MarketAnalyzer.App
 {
@@ -7,8 +8,9 @@ namespace Recodme.Labs.MarketAnalyzer.App
     {
         static void Main(string[] args)
         {
-            var ctx = new Context();
-            ctx.Database.EnsureCreated();
+            //var ctx = new Context();
+            //ctx.Database.EnsureCreated();
+
         }
     }
 }
---
+++ Recodme.Labs.MarketAnalyzer.Scrapping/Scrap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace Recodme.Labs.MarketAnalyzer.Scrapping
+{
+    public class Scrap
+    {
+        public void GetInfo()
+        {
+            var wc = new WebClient();
+            string page = wc.DownloadString("https://www.slickcharts.com/sp500");
+
+            var htmlDocument = new HtmlAgilityPack.HtmlDocument();
+            htmlDocument.LoadHtml(page);
+
+
+        }
+    }
+}
