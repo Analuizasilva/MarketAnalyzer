34d6972c18e2a578065b102cc9cd1972e3234e61
s-cunha <s.cunha@recodme.pt>
2020-08-04 11:35:22 +0100

    Changes to DataAccessObject

---
+++ Recodme.Labs.MarketAnalyzer.DataAccessLayer/Base/BaseDataAccessObject.cs
@@ -0,0 +1,104 @@
+﻿using DataAccessLayer.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Recodme.Labs.MarketAnalyzer.DataLayer.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recodme.Labs.MarketAnalyzer.DataAccessLayer.Base
+{
+    public class BaseDataAccessObject<T> where T : Entity
+    {
+        private Context _context;
+        public BaseDataAccessObject()
+        {
+            _context = new Context();
+        }
+        #region Create
+        public void Create(T item)
+        {
+            using (var _ctx = new Context())
+            {
+                _ctx.Set<T>().Add(item);
+                _ctx.SaveChanges();
+            }
+        }
+        public async Task CreateAsync(T item)
+        {
+            using (var _ctx = new Context())
+            {
+                await _context.Set<T>().AddAsync(item);
+                await _context.SaveChangesAsync();
+            }
+        }
+        #endregion
+
+        #region Read
+        public T Read(Guid id)
+        {
+            return _context.Set<T>().FirstOrDefault(x => x.Id == id);
+        }
+        public async Task<T> ReadAsync(Guid id)
+        {
+            return await _context.Set<T>().FirstOrDefaultAsync(x => x.Id == id);
+        }
+        #endregion
+
+        #region Update
+        public void Update(T item)
+        {
+            using (var _ctx = new Context())
+            {
+                _ctx.Entry(item).State = EntityState.Modified;
+                _ctx.SaveChanges();
+            }
+        }
+        public async Task UpdateAsync(T item)
+        {
+            using (var _ctx = new Context())
+            {
+                _ctx.Entry(item).State = EntityState.Modified;
+                await _ctx.SaveChangesAsync();
+            }
+        }
+        #endregion
+
+        #region Delete
+        public void Delete(T item)
+        {
+            item.IsDeleted = true;
+            Update(item);
+        }
+        public void Delete(Guid id)
+        {
+            var item = Read(id);
+            if (item == null) return;
+            Delete(item);
+        }
+        public async Task DeleteAsync(T item)
+        {
+            item.IsDeleted = true;
+            await UpdateAsync(item);
+        }
+        public async Task DeleteAsync(Guid id)
+        {
+            var item = ReadAsync(id).Result;
+            if (item == null) return;
+            await DeleteAsync(item);
+        }
+        #endregion
+
+        #region List
+        public List<T> List()
+        {
+            return _context.Set<T>().ToList();
+        }
+        public async Task<List<T>> ListAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+        #endregion
+    }
+}
