ad84de57162e1cba6e5fbc80467479ad74ff64dd
TMVAntunes <61155054+TMVAntunes@users.noreply.github.com>
2020-08-04 09:11:30 +0100

    Suo problem, comment Program.

--- Recodme.Labs.MarketAnalyzer.App/Program.cs
+++ Recodme.Labs.MarketAnalyzer.App/Program.cs
@@ -14,30 +14,30 @@ namespace Recodme.Labs.MarketAnalyzer.App
             var _ctx = new Context();
             _ctx.Database.EnsureCreated();
 
-            HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
-            HtmlAgilityPack.HtmlDocument doc = web.Load("https://www.slickcharts.com/sp500");
+            //HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
+            //HtmlAgilityPack.HtmlDocument doc = web.Load("https://www.slickcharts.com/sp500");
 
-            var headerContent = doc.DocumentNode
-                .SelectNodes("//table[@class='table table-hover table-borderless table-sm']").Descendants("td").ToList();
+            //var headerContent = doc.DocumentNode
+            //    .SelectNodes("//table[@class='table table-hover table-borderless table-sm']").Descendants("td").ToList();
 
-            var listOfCompanies = new List<Company>();
+            //var listOfCompanies = new List<Company>();
 
-            for (int i = 0; i < headerContent.Count() / 7; i++)
-            {
-                var count = i * 7;
-                //Console.WriteLine($"{headerContent[count].InnerText} | {headerContent[count + 1].InnerText} |{headerContent[count + 2].InnerText} |{headerContent[count + 4].InnerText} ");
+            //for (int i = 0; i < headerContent.Count() / 7; i++)
+            //{
+            //    var count = i * 7;
+            //    //Console.WriteLine($"{headerContent[count].InnerText} | {headerContent[count + 1].InnerText} |{headerContent[count + 2].InnerText} |{headerContent[count + 4].InnerText} ");
 
-                var _rank = Convert.ToInt32(headerContent[count].InnerText);
+            //    var _rank = Convert.ToInt32(headerContent[count].InnerText);
 
-                var _priceString = headerContent[count + 4].InnerText;
-                var _price = Convert.ToDouble(_priceString.Remove(0, 13));
+            //    var _priceString = headerContent[count + 4].InnerText;
+            //    var _price = Convert.ToDouble(_priceString.Remove(0, 13));
 
-                var _company = new Company(headerContent[count + 1].InnerText, headerContent[count + 2].InnerText, _rank, _price);
+            //    var _company = new Company(headerContent[count + 1].InnerText, headerContent[count + 2].InnerText, _rank, _price);
              
-                listOfCompanies.Add(_company);
-                _ctx.Companies.AddRange(listOfCompanies);
+            //    listOfCompanies.Add(_company);
+            //    _ctx.Companies.AddRange(listOfCompanies);
 
-            }
+            //}
 
         }
     }
