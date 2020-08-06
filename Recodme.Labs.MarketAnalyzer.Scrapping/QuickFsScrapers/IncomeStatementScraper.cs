9ec537b0fa85b9348f501c4fac5ed8f9473b4976
s-cunha <s.cunha@recodme.pt>
2020-08-06 16:55:44 +0100

    small change to IncomeStatementScraper

--- Recodme.Labs.MarketAnalyzer.Scrapping/QuickFsScrapers/IncomeStatementScraper.cs
+++ Recodme.Labs.MarketAnalyzer.Scrapping/QuickFsScrapers/IncomeStatementScraper.cs
@@ -13,22 +13,19 @@ namespace Recodme.Labs.MarketAnalyzer.Scraping.QuickFsScrapers
 {
     public class IncomeStatementScraper
     {
-        public class IncomeStatementScraper
+        public async Task<HtmlNode> ScrapeIncomeStatement()
         {
-            public async Task<HtmlNode> ScrapeIncomeStatement()
-            {
-                var helper = new WebHelper();
-                var request = await helper.ComposeWebRequestGet("https://api.quickfs.net/stocks/AAPL:US/is/Annual/grL0gNYoMoLUB1ZoAKLfhXkoMoLODiO1WoL9.grLtk3PoMoLmqFEsMasbNK9fkXudkNBtR2jpkr5dINZoAKLtRNZoMlG1MJR3PQP1PlxcOpEfqXGoMwcoqNWaka9tIKO6OlGnPiYsOosoIS1fySsoMoLiApW1hpffZFLaR29uhSDdkFZoAKLsRNWiq29rIKO6OpLcqSBQJ0ZrPCOcOwHryNIthXBwICO6PKsokpBwyS9dDFLtqoO6grLBDrO6PCsoZ0GoMlH9vN0.4clnWa197BohIJjcOe14FjaQaoJ9aGymU9SIOGqOFku");
-                var result = await helper.CallWebRequest(request);
-                result = result.Replace("<\\/td>", "</td>");
+            var helper = new WebHelper();
+            var request = await helper.ComposeWebRequestGet("https://api.quickfs.net/stocks/AAPL:US/is/Annual/grL0gNYoMoLUB1ZoAKLfhXkoMoLODiO1WoL9.grLtk3PoMoLmqFEsMasbNK9fkXudkNBtR2jpkr5dINZoAKLtRNZoMlG1MJR3PQP1PlxcOpEfqXGoMwcoqNWaka9tIKO6OlGnPiYsOosoIS1fySsoMoLiApW1hpffZFLaR29uhSDdkFZoAKLsRNWiq29rIKO6OpLcqSBQJ0ZrPCOcOwHryNIthXBwICO6PKsokpBwyS9dDFLtqoO6grLBDrO6PCsoZ0GoMlH9vN0.4clnWa197BohIJjcOe14FjaQaoJ9aGymU9SIOGqOFku");
+            var result = await helper.CallWebRequest(request);
+            result = result.Replace("<\\/td>", "</td>");
 
-                HtmlDocument html = new HtmlDocument();
-                html.LoadHtml(result);
+            HtmlDocument html = new HtmlDocument();
+            html.LoadHtml(result);
 
-                var htmlNodes = html.DocumentNode.Descendants().Where(x => x.Name == "tbody").SingleOrDefault();
-                Console.WriteLine(htmlNodes.InnerText);
-                return htmlNodes;
-            }
+            var htmlNodes = html.DocumentNode.Descendants().Where(x => x.Name == "tbody").SingleOrDefault();
+            Console.WriteLine(htmlNodes.InnerText);
+            return htmlNodes;
         }
     }
 }
