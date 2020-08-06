91ffdbf90ede5a95cafe6ad641216d22fbbd5da1
Analuizasilva <a.silva@recodme.pt>
2020-08-06 15:50:39 +0100

    .suo

---
+++ Recodme.Labs.MarketAnalyzer.Scrapping/QuickFsScrappers/BalanceSheetScrapper.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using Recodme.Labs.MarketAnalyzer.DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Net;
+namespace Recodme.Labs.MarketAnalyzer.Scrapping.QuickFsScrappers
+{
+    public class BalanceSheetScrapper
+    {
+      
+
+
+
+   
+    }
+}
