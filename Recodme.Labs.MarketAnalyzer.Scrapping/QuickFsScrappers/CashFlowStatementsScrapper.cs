229688f9ef95b391592bf0a12e12c7ae4da5fe79
TMVAntunes <61155054+TMVAntunes@users.noreply.github.com>
2020-08-06 15:52:14 +0100

    suo

--- Recodme.Labs.MarketAnalyzer.Scrapping/QuickFsScrappers/CashFlowStatementsScrapper.cs
+++ Recodme.Labs.MarketAnalyzer.Scrapping/QuickFsScrappers/CashFlowStatementsScrapper.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json.Linq;
+using Recodme.Labs.MarketAnalyzer.DataLayer;
 using System;
 using System.Collections.Generic;
 using System.Net;
@@ -8,7 +9,7 @@ namespace Recodme.Labs.MarketAnalyzer.Scrapping.QuickFsScrappers
 {
     class CashFlowStatementsScrapper
     {
-        public List<BalanceSheets> ScrapeBalanceSheet()
+        public List<CashFlowStatement> ScrapeCashFlowStatement()
         {
             var wc = new WebClient();
             string page = wc.DownloadString("https://api.quickfs.net/stocks/MSFT:US/bs/Annual/grL0gNYoMoLUB1ZoAKLfhXkoMoLODiO1WoL9.grLtk3PoMoLmqFEsMasbNK9fkXudkNBtR2jpkr5dINZoAKLtRNZoMlG1MJR3PQk0PiRcOpEfqXGoMwcoqNWaka9tIKO6OlGnPiYiOosoIS1fySsoMoLfAwWthFIfZFLaR29uhSDdkFZoAKLsRNWiq29rIKO6OlPrWQDrWlx4OosokFLtqpacISqaOlmsAKLrISqth25Zkpa2Olt7OaBJOlmnAKLQZCO6PF19vZ.4Cln1o9anX5WXxb47nHBsRfwL7J-rMp073IE-QEfpJZ");
