b85caadcb3f3db158f003c60a11342708ca8ed6d
TMVAntunes <61155054+TMVAntunes@users.noreply.github.com>
2020-08-06 16:51:04 +0100

    Modified CashFlowStatementsScrapper

--- Recodme.Labs.MarketAnalyzer.Scrapping/QuickFsScrappers/CashFlowStatementsScrapper.cs
+++ Recodme.Labs.MarketAnalyzer.Scrapping/QuickFsScrappers/CashFlowStatementsScrapper.cs
@@ -1,32 +1,27 @@
-﻿using Newtonsoft.Json.Linq;
-using Recodme.Labs.MarketAnalyzer.DataLayer;
-using System;
-using System.Collections.Generic;
-using System.Net;
-using System.Text;
+﻿using HtmlAgilityPack;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Recodme.Labs.MarketAnalyzer.Scrapping.QuickFsScrappers
 {
     class CashFlowStatementsScrapper
     {
-        public List<CashFlowStatement> ScrapeCashFlowStatement()
+        public async Task<HtmlNode> ScrapeCashFlowStatements()
         {
-            var wc = new WebClient();
-            string page = wc.DownloadString("https://api.quickfs.net/stocks/MSFT:US/bs/Annual/grL0gNYoMoLUB1ZoAKLfhXkoMoLODiO1WoL9.grLtk3PoMoLmqFEsMasbNK9fkXudkNBtR2jpkr5dINZoAKLtRNZoMlG1MJR3PQk0PiRcOpEfqXGoMwcoqNWaka9tIKO6OlGnPiYiOosoIS1fySsoMoLfAwWthFIfZFLaR29uhSDdkFZoAKLsRNWiq29rIKO6OlPrWQDrWlx4OosokFLtqpacISqaOlmsAKLrISqth25Zkpa2Olt7OaBJOlmnAKLQZCO6PF19vZ.4Cln1o9anX5WXxb47nHBsRfwL7J-rMp073IE-QEfpJZ");
-            JObject jsonDocument = JObject.Parse(page);
-            StringBuilder sb = new StringBuilder();
-            foreach (JProperty prop in jsonDocument["datasets"].Children<JProperty>())
-            {
-                JObject data = (JObject)prop.Value;
-                sb.AppendFormat("<img src='{0}' alt='{1}' />\r\n",
-                                 data["bs"]);
-            }
-            Console.WriteLine(sb.ToString());
-        }
-        
+            var helper = new WebHelper();
+            var request = await helper.ComposeWebRequestGet("https://api.quickfs.net/stocks/MSFT:US/cf/Annual/grL0gNYoMoLUB1ZoAKLfhXkoMoLODiO1WoL9.grLtk3PoMoLmqFEsMasbNK9fkXudkNBtR2jpkr5dINZoAKLtRNZoMlG1MJR3PQk0PiRcOpEfqXGoMwcoqNWaka9tIKO6OlGnPiYiOosoIS1fySsoMoLfAwWthFIfZFLaR29uhSDdkFZoAKLsRNWiq29rIKO6OlPrWQDrWlx4OosokFLtqpacISqaOlmsAKLrISqth25Zkpa2Olt7OaBJOlmnAKLQZCO6PF19vZ.4Cln1o9anX5WXxb47nHBsRfwL7J-rMp073IE-QEfpJZ");
 
-        
-    
+            var result = await helper.CallWebRequest(request);
+            result = result.Replace("<\\/td>", "</td>");
+
+            HtmlAgilityPack.HtmlDocument html = new HtmlAgilityPack.HtmlDocument();
+            html.LoadHtml(result);
+
+            var htmlNodes = html.DocumentNode.Descendants().Where(x => x.Name == "tbody").SingleOrDefault();
+
+            return htmlNodes;
+        }
+
     }
 
 }
