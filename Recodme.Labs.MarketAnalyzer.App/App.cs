1dd04571936fd3845507b9401ad97528d86b6047
s-cunha <s.cunha@recodme.pt>
2020-08-07 17:09:25 +0100

    Changes to IncomeStatementScraper

--- Recodme.Labs.MarketAnalyzer.App/App.cs
+++ Recodme.Labs.MarketAnalyzer.App/App.cs
@@ -22,26 +22,9 @@ namespace Recodme.Labs.MarketAnalyzer.App
     {
         public async Task Run()
         {
-            /* Instanciar um web client
-             * fazer request para o URL com o webclient
-             * converter a string resultado num objecto JSON
-             * fazer parse ao JSON para extrair o HTML que está na propriedade
-             * Instanciar o HTML document passando como parametro de entrada a string que contem o HTML
-             *extrair a informação direitinha */
-
-            var balanceSheetScraper = new BalanceSheetScraper();
-            await balanceSheetScraper.ScrapeBalanceSheet();
-
-            // var slickChartsBO = new SlickChartsBO();
-            //await slickChartsBO.ScrapeAndStoreData();
-
-            //var ctx = new Context();
-            //ctx.Database.EnsureCreated();
-
-            //var scrap = new SlickChartsScrapper();
-
-            //var bo = new BusinessObject<Company>();
-            //bo.AddAndUpdateCompanies(scrap.ScrapeCompanies());
+            var incomestatementScraper = new IncomeStatementScraper();
+            var result = await incomestatementScraper.ScrapeIncomeStatement();
+            await incomestatementScraper.OrganizeScrapedIncomeStatement(result);
         }
     }
 }
