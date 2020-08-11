f2c2eeeee6ecea9147636b7b1f1f7db7df5266ea
s-cunha <s.cunha@recodme.pt>
2020-08-11 15:36:14 +0100

    changes to incomeStatementScraper - vertical scraping

--- Recodme.Labs.MarketAnalyzer.Scrapping/QuickFsScrapers/IncomeStatementScraper.cs
+++ Recodme.Labs.MarketAnalyzer.Scrapping/QuickFsScrapers/IncomeStatementScraper.cs
@@ -17,11 +17,8 @@ namespace Recodme.Labs.MarketAnalyzer.Scraping.QuickFsScrapers
 {
     public class IncomeStatementScraper
     {
-        public async Task ScrapeIncomeStatement(string ticker)
+        public async Task<List<ExtractedValues>> ScrapeIncomeStatement(string ticker)
         {
-            
-            var extractedValuesList = new List<ExtractedValues>();
-
             string url = "https://api.quickfs.net/stocks/" + ticker + ":US/is/Annual/grL0gNYoMoLUB1ZoAKLfhXkoMoLODiO1WoL9.grLtk3PoMoLmqFEsMasbNK9fkXudkNBtR2jpkr5dINZoAKLtRNZoMlG1MJR3PQP1PlxcOpEfqXGoMwcoqNWaka9tIKO6OlGnPiYsOosoIS1fySsoMoLiApW1hpffZFLaR29uhSDdkFZoAKLsRNWiq29rIKO6OpLcqSBQJ0ZrPCOcOwHryNIthXBwICO6PKsokpBwyS9dDFLtqoO6grLBDrO6PCsoZ0GoMlH9vN0.4clnWa197BohIJjcOe14FjaQaoJ9aGymU9SIOGqOFku";
 
             var helper = new WebHelper();
@@ -29,6 +26,8 @@ namespace Recodme.Labs.MarketAnalyzer.Scraping.QuickFsScrapers
 
             var result = await helper.CallWebRequest(request);
             result = result.Replace("<\\/td>", "");
+            result = result.Replace("$", "");
+            result = result.Replace(",", ".");
 
             var html = new HtmlAgilityPack.HtmlDocument();
             html.LoadHtml(result);
@@ -37,35 +36,47 @@ namespace Recodme.Labs.MarketAnalyzer.Scraping.QuickFsScrapers
 
             var numberOfColumns = html.DocumentNode.SelectNodes("//tr[@class='thead']").Descendants("td").ToList().Count();
             var numberOfRows = htmlNodes.Count / numberOfColumns;
+            var count = 1;
+
+            var extractedValuesList = new List<ExtractedValues>();
 
             for (var i = 1; i < numberOfColumns; i++)
             {
-   
-                var extractedValues = new ExtractedValues();
-                var baseItems = new BaseItem();
-
                 var parsedYear = int.TryParse(htmlNodes[i].InnerText, out int yearNumber);
-                if (!parsedYear) return;
-
-                if (!(yearNumber == 0))
-                {
-                    extractedValues.Year = yearNumber;
-                }
-                var count = 1;
+                //if (!parsedYear) return; lançar exceção
+               
                 for (var j = 1; j < numberOfRows; j++)
                 {
+                    var extractedValues = new ExtractedValues();
+                    var baseItems = new BaseItem();
+
                     var name = htmlNodes[j * numberOfColumns].InnerText;
                     baseItems.Name = name;
-                    
-                    //var valuesPerYear = htmlNodes[(j * numberOfColumns)+count].InnerText;
 
-                    Console.WriteLine(yearNumber+ " "+ name /*+ " " + valuesPerYear*/);
-                    count++;
+                    var valuesList = new List<string>();
+                    foreach(var item in htmlNodes)
+                    {
+                        var htmlValue = item.GetAttributeValue("data-value", null);
+                        valuesList.Add(htmlValue);
+                    }
+
+                    var valuesFromNodes = valuesList[(j * numberOfColumns) + count];
+                    bool parsedFloat = float.TryParse(valuesFromNodes, NumberStyles.Float, CultureInfo.InvariantCulture, out float valuesFloat);
+
+                    if (yearNumber != 0 && name != "" && name!="Operating Expenses")
+                    {
+                        extractedValues.Year = yearNumber;
+                        baseItems.Name = name;
+                        baseItems.Value = valuesFloat;
+                        extractedValues.Items.Add(baseItems);
+                        extractedValuesList.Add(extractedValues);
+                        Console.WriteLine(extractedValues.Year + " " + baseItems.Name + " " + baseItems.Value);
+                    }
                 }
-               
-                if (!(yearNumber == 0)) extractedValuesList.Add(extractedValues);
+                count++;
+                
             }
-
+            return extractedValuesList;
         }
     }
 }
