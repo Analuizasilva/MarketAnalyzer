fedc7289881196b03224f2be3ce92cafe837e954
Marco Pereira <m.pereira@recodme.pt>
2020-08-12 14:37:56 +0100

    KeyRatioScraper Updated

--- Recodme.Labs.MarketAnalyzer.Scrapping/QuickFsScrapers/Base/ExtractedValues.cs
+++ Recodme.Labs.MarketAnalyzer.Scrapping/QuickFsScrapers/Base/ExtractedValues.cs
@@ -6,7 +6,7 @@ namespace Recodme.Labs.MarketAnalyzer.Scrapping.QuickFsScrapers.Base
 {
     public class ExtractedValues
     {
-        public string Year { get; set; }
+        public int Year { get; set; }
         public List<BaseItem> Items { get; set; }
 
         public ExtractedValues()
