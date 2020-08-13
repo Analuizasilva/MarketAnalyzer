c18a3066c4defcd0d5eaf0dc4db29fd2941a9c95
s-cunha <s.cunha@recodme.pt>
2020-08-13 13:35:17 +0100

    Changes to IncomeStatement, IncomeStatementScraper, IncomeStatementBO

--- Recodme.Labs.MarketAnalyzer.BusinessLayer/BusinessObjects/QuickFS/IncomeStatementBO.cs
+++ Recodme.Labs.MarketAnalyzer.BusinessLayer/BusinessObjects/QuickFS/IncomeStatementBO.cs
@@ -1,4 +1,6 @@
-﻿using Recodme.Labs.MarketAnalyzer.Scraping.QuickFsScrapers;
+﻿using Recodme.Labs.MarketAnalyzer.DataLayer;
+using Recodme.Labs.MarketAnalyzer.Scraping.QuickFsScrapers;
+using Recodme.Labs.MarketAnalyzer.Scraping.SlickChartsScrapers;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,6 +10,34 @@ namespace Recodme.Labs.MarketAnalyzer.BusinessLayer.BusinessObjects.QuickFS
 {
     public class IncomeStatementBO
     {
-        
+        public async Task<List<List<IncomeStatement>>> ScrapeAllIncomeStatements()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1));
+            var slickChartsScraper = new SlickChartsScraper();
+            var companies = slickChartsScraper.ScrapeCompanies();
+
+            var incomeStatementScraper = new IncomeStatementScraper();
+
+            var allIncomeStatements = new List<List<IncomeStatement>>();
+
+            foreach (var company in companies)
+            {
+                if (company.Ticker != "OXY.WT")
+                {
+                    var ticker = company.Ticker;
+
+                    var incomeStatement = await incomeStatementScraper.ScrapeIncomeStatement(ticker);
+                    foreach (var incs in incomeStatement)
+                    {
+                        //incs.CompanyId = company.Id; mudar para o company da base de dados
+                        Console.WriteLine(ticker + " " + incs.Year + " " + incs.Revenue);
+                    }
+                    allIncomeStatements.Add(incomeStatement);
+                }
+            }
+
+            return allIncomeStatements;
+        }
+
     }
 }
