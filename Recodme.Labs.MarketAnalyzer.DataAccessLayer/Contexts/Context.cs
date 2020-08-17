1a14d5c91cfb9bcab225aa2728c6da2944645e2f
s-cunha <s.cunha@recodme.pt>
2020-08-17 15:51:53 +0100

    Changes to overall project to address errors

--- Recodme.Labs.MarketAnalyzer.DataAccessLayer/Contexts/Context.cs
+++ Recodme.Labs.MarketAnalyzer.DataAccessLayer/Contexts/Context.cs
@@ -1,48 +1,48 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-using Recodme.Labs.MarketAnalyzer.DataAccessLayer.Properties;
-using Recodme.Labs.MarketAnalyzer.DataLayer;
+﻿//using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore;
+//using Recodme.Labs.MarketAnalyzer.DataAccessLayer.Properties;
+//using Recodme.Labs.MarketAnalyzer.DataLayer;
 
-namespace DataAccessLayer.Contexts
-{
-    public class Context : IdentityDbContext
-    {
-        private readonly string _connectionString = string.Empty;
+//namespace DataAccessLayer.Contexts
+//{
+//    public class Context : IdentityDbContext
+//    {
+//        private readonly string _connectionString = string.Empty;
 
-        public Context() : base() { }
+//        public Context() : base() { }
 
-        public Context(DbContextOptions<Context> options) : base(options)
-        {
+//        public Context(DbContextOptions<Context> options) : base(options)
+//        {
 
-        }
+//        }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-                if (_connectionString == string.Empty)
-                {
-                    optionsBuilder.UseSqlServer(Resources.ConnectionString);
-                }
-                else
-                {
-                    optionsBuilder.UseSqlServer(_connectionString);
-                }
-            }
-        }
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//                if (_connectionString == string.Empty)
+//                {
+//                    optionsBuilder.UseSqlServer(Resources.ConnectionString);
+//                }
+//                else
+//                {
+//                    optionsBuilder.UseSqlServer(_connectionString);
+//                }
+//            }
+//        }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-        }
+//        protected override void OnModelCreating(ModelBuilder modelBuilder)
+//        {
+//            base.OnModelCreating(modelBuilder);
+//        }
 
-        public DbSet<Company> Companies { get; set; }
-        public DbSet<BalanceSheet> BalanceSheets { get; set; }
-        public DbSet<CashFlowStatement> CashFlowStatements { get; set; }
-        public DbSet<CashFlowStatementTTM> CashFlowStatementsTTM { get; set; }
-        public DbSet<IncomeStatement> IncomeStatements { get; set; }
-        public DbSet<IncomeStatementTTM> IncomeStatementsTTM { get; set; }
-        public DbSet<KeyRatio> KeyRatios { get; set; }
+//        public DbSet<Company> Companies { get; set; }
+//        public DbSet<BalanceSheet> BalanceSheets { get; set; }
+//        public DbSet<CashFlowStatement> CashFlowStatements { get; set; }
+//        public DbSet<CashFlowStatementTTM> CashFlowStatementsTTM { get; set; }
+//        public DbSet<IncomeStatement> IncomeStatements { get; set; }
+//        public DbSet<IncomeStatementTTM> IncomeStatementsTTM { get; set; }
+//        public DbSet<KeyRatio> KeyRatios { get; set; }
 
-    }
-}
+//    }
+//}
