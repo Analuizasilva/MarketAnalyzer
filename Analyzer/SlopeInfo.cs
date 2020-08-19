8d73c72a6573b56cb7ac7c24960bca73225ac1c4
Marco Pereira <m.pereira@recodme.pt>
2020-08-19 13:08:57 +0100

    SlipeInfo Updated

--- Analyzer/SlopeInfo.cs
+++ Analyzer/SlopeInfo.cs
@@ -3,6 +3,7 @@ using Recodme.Labs.MarketAnalyzer.BusinessLayer.BusinessObjects;
 using Recodme.Labs.MarketAnalyzer.DataLayer;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Recodme.Labs.MarketAnalyzer.Analysis
@@ -11,12 +12,13 @@ namespace Recodme.Labs.MarketAnalyzer.Analysis
     {
         //o que um slopeInfo recebe como parâmetro é a lista de extractedValues
 
-        public List<ExtractedValue> CalculateEquityGrowth(Guid companyId)
+        public List<double> CalculateEquityGrowth(Guid companyId)
         {
             var bo = new DataBaseBusinessObject();
             var balanceSheets = bo.GetBalanceSheets().Result;
             var companies = bo.GetCompanies().Result;
             var equity = new List<ExtractedValue>();
+
             foreach (var item in companies)
             {
                 if (item.Id == companyId)
@@ -26,9 +28,20 @@ namespace Recodme.Labs.MarketAnalyzer.Analysis
                 }
             }
 
+            List<ExtractedValue> sortedList = equity.OrderBy(o => o.Year).ToList();
+            var equityGrowthRate = new List<double>();
+            for (var i = 0; i < (sortedList.Count -1); i++ )
+            {
+                var beginningValue = sortedList[i].Value;
+                var finalValue = sortedList[i + 1].Value;
+                var result = (double)(finalValue / beginningValue);
+                float years = sortedList.Count;
+                float pow = 1 / years;
+                var growth = Math.Pow(result, pow)-1;
 
-
-            return equity;
+                equityGrowthRate.Add(growth);
+            }
+            return equityGrowthRate;
         }
 
 
