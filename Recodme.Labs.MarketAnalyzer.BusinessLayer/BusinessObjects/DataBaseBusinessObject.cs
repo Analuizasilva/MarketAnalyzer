e339367c75a49d777348e05230a40cc58883c4e2
s-cunha <s.cunha@recodme.pt>
2020-08-19 11:33:33 +0100

    Changes to DataBaseBO and DataAccess

--- Recodme.Labs.MarketAnalyzer.BusinessLayer/BusinessObjects/DataBaseBusinessObject.cs
+++ Recodme.Labs.MarketAnalyzer.BusinessLayer/BusinessObjects/DataBaseBusinessObject.cs
@@ -4,27 +4,54 @@ using Recodme.Labs.MarketAnalyzer.DataLayer.Base;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Recodme.Labs.MarketAnalyzer.BusinessLayer.BusinessObjects
 {
     public class DataBaseBusinessObject
     {
 
-        public List<ExtractedBalanceSheet> GetBalanceSheets()
+        public async Task<List<ExtractedBalanceSheet>> GetBalanceSheets()
         {
             var dao = new BaseDataAccessObject<ExtractedBalanceSheet>();
-            return dao.List();
+            return await dao.ListAsync();
         }
 
-        public void GetDataBaseData()
+        public async Task<List<Company>> GetCompanies()
         {
-
-
-
-            var result = new BaseDataAccessObject<T>();
-
-           // result.Create()
-
+            var dao = new BaseDataAccessObject<Company>();
+            return await dao.ListAsync();
         }
+        public async Task<List<ExtractedCashFlowStatement>> GetCashFlowStatements()
+        {
+            var dao = new BaseDataAccessObject<ExtractedCashFlowStatement>();
+            return await dao.ListAsync();
+        }
+
+        public async Task<List<ExtractedCashFlowStatementTtm>> GetCashFlowStatementTtms()
+        {
+            var dao = new BaseDataAccessObject<ExtractedCashFlowStatementTtm>();
+            return await dao.ListAsync();
+        }
+
+        public async Task<List<ExtractedIncomeStatement>> GetIncomeStatements()
+        {
+            var dao = new BaseDataAccessObject<ExtractedIncomeStatement>();
+            return await dao.ListAsync();
+        }
+
+        public async Task<List<ExtractedIncomeStatementTtm>> GetIncomeStatementTtms()
+        {
+            var dao = new BaseDataAccessObject<ExtractedIncomeStatementTtm>();
+            return await dao.ListAsync();
+        }
+
+        public async Task<List<ExtractedKeyRatio>> GetKeyRatios()
+        {
+            var dao = new BaseDataAccessObject<ExtractedKeyRatio>();
+            return await dao.ListAsync();
+        }
+
+
     }
 }
