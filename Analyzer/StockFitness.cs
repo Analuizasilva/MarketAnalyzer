68c444b4d9e5bfe24ebe92aece93f5afdb8540e6
Analuizasilva <a.silva@recodme.pt>
2020-08-21 17:11:35 +0100

    Changes in the StockFitness class

--- Analyzer/StockFitness.cs
+++ Analyzer/StockFitness.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Recodme.Labs.MarketAnalyzer.DataLayer.Pocos;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,15 +7,56 @@ namespace Recodme.Labs.MarketAnalyzer.Analysis
 {
     public class StockFitness
     {
+
         /* Viabilidade de cada empresa
-        fitness do roic, equity Growth, EPS Growth, Revenue Growth, PE Ratio, Debt to Equity, Assets to Liabilities
-        um somatório dos valores e dar um valor total da viabilidade a cada empresa.
+fitness do roic, equity Growth, EPS Growth, Revenue Growth, PE Ratio, Debt to Equity, Assets to Liabilities
+um somatório dos valores e dar um valor total da viabilidade a cada empresa.
 
-        O sistema deve conseguir atribuir um peso decidido pelo utilizador a cada atributo.
+O sistema deve conseguir atribuir um peso decidido pelo utilizador a cada atributo.
 
-        */
-        public double? Roic { get; set; }
+*/
+
+        public double? RoicFitness { get; set; }
+        public double? EquityFitness { get; set; }
+        public double? EPSFitness { get; set; }
+        public double? RevenueFitness { get; set; }
+        public double? PERatioFitness { get; set; }
+        public double? DebtToEquityFitness { get; set; }
+        public double? AssetsToLiabilitiesFitness { get; set; }
+        public double? TotalFitness { get; set; }
+        public double WeightNumber { get; set; }
 
 
+        public StockFitness(CompanyDataPoco dataPoco)
+        {
+            var analysis = new StockAnalysis(dataPoco);
+            #region RoicFitness
+            var roicSlope = analysis.RoicSlopeInfo.GrowthTrendline.Slope;
+            var roicDeviation = analysis.RoicSlopeInfo.GrowthDeviation;
+            var ratio = Math.Abs((double)(roicDeviation / roicSlope));
+            double? roicFitness = 0;
+            if (ratio >= 1) roicFitness = 1 / ratio;
+            else roicFitness = 1;
+            #endregion
+
+            double equityFitness = 0;
+            var equityAbsSlope = analysis.EquitySlopeInfo.GrowthTrendline.Slope;
+            if (equityAbsSlope < 0) equityFitness = 0;
+            else if(equityAbsSlope<1) equityFitness =0.5;
+            else equityFitness = 1-1/equityAbsSlope;
+
+            var ePSSlope = analysis.EPSSlopeInfo.GrowthTrendline.Slope;
+            var revenueSlope = analysis.RevenueSlopeInfo.GrowthTrendline.Slope;
+
+            var peRatio = analysis.PERatio;
+
+            var debtToEquity = analysis.DebtToEquity;
+
+            var assetsToLiabilities = analysis.AssetsToLiabilities;
+
+            RoicFitness = roicFitness;
+            EquityFitness = analysis.Equity;
+
+        }
     }
 }
--- Recodme.Labs.MarketAnalyzer.App/App.cs
+++ Recodme.Labs.MarketAnalyzer.App/App.cs
@@ -3,6 +3,7 @@ using Recodme.Labs.MarketAnalyzer.Analysis;
 using Recodme.Labs.MarketAnalyzer.DataAccessLayer;
 using Recodme.Labs.MarketAnalyzer.DataAccessLayer.Base;
 using Recodme.Labs.MarketAnalyzer.DataLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -15,17 +16,34 @@ namespace Recodme.Labs.MarketAnalyzer.App
         {
             var dao = new CompanyDataAccessObject();
             var result = dao.GetCompaniesInfo();
-            var apple = result.FirstOrDefault(x => x.Company.Ticker == "AAPL");
+            var apple = result.FirstOrDefault(x => x.Company.Ticker == "AIPBF");
 
             var financial = new FinancialAnalysis();
             var equity = financial.GetEquity(apple);
 
             var slopeinfo = new SlopeInfo(equity);
-            var trendline = slopeinfo.NominalTrendline;
+            var growthTrendline = slopeinfo.GrowthTrendline;
             var growth = slopeinfo.Growth;
 
             var stockAnalysisApple = new StockAnalysis(apple);
-           
+
+            double equityFitness = 0;
+            var equityAbsSlope = stockAnalysisApple.EquitySlopeInfo.GrowthTrendline.Slope;
+            if (equityAbsSlope < 0) equityFitness = 0;
+            else if (equityAbsSlope < 1) equityFitness = 0.5;
+            else equityFitness = 1 - 1 / equityAbsSlope;
+
+
+            var growthEquityTrendline = stockAnalysisApple.EquitySlopeInfo.GrowthTrendline;
+
+
+            var roicSlope = stockAnalysisApple.RoicSlopeInfo.NominalTrendline.Slope;
+            var roicDeviation = stockAnalysisApple.RoicSlopeInfo.NominalDeviation;
+            var ratio = Math.Abs((double)(roicDeviation / roicSlope));
+            double? roicFitness = 0;
+            if (ratio >= 1) roicFitness = 1 / ratio;
+            else roicFitness = 1;
+
             //var companyDAO = new BaseDataAccessObject<Company>();
             //var companyDB = companyDAO.ListAsync().Result;
             //var info = companyDAO.GetCompaniesInfo();
