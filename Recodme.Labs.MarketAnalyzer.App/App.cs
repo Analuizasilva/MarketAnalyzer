806ea5f83212d7da2132832d7151810a2afc676d
s-cunha <s.cunha@recodme.pt>
2020-08-26 11:06:13 +0100

    Added GetStockFitness to businessLayer

--- Recodme.Labs.MarketAnalyzer.App/App.cs
+++ Recodme.Labs.MarketAnalyzer.App/App.cs
@@ -4,6 +4,7 @@ using Recodme.Labs.MarketAnalyzer.Analysis.Support;
 using Recodme.Labs.MarketAnalyzer.DataAccessLayer;
 using Recodme.Labs.MarketAnalyzer.DataAccessLayer.Base;
 using Recodme.Labs.MarketAnalyzer.DataLayer;
+using Recodme.Labs.MarketAnalyzer.DataLayer.Pocos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,19 +18,31 @@ namespace Recodme.Labs.MarketAnalyzer.App
         {
             var dao = new CompanyDataAccessObject();
             var companiesDataPoco = dao.GetCompaniesInfo();
-            
-
-
+            var count = 0;
+            var list = new List<Poco>();
             foreach (var companyDataPoco in companiesDataPoco)
             {
                 var stockAnalysis = new StockAnalysis(companyDataPoco);
                 var stockFitness = new StockFitness(stockAnalysis);
                 var total = stockFitness.TotalFitness;
-                Console.WriteLine(companyDataPoco.Company.Ticker + " "+ total);
+                if (total != null)
+                {
+                    list.Add(new Poco { CP = companyDataPoco, Fitness = total });
+                    count++;
+                }
+                //Console.WriteLine(companyDataPoco.Company.Ticker + " " + total);
             }
-
-
-
+            foreach (var item in list.OrderBy(l => l.Fitness))
+            {
+                Console.WriteLine(item.CP.Company.Ticker + " " + item.Fitness);
+            }
+            Console.WriteLine(count + " " + "Companies");
         }
+        public class Poco
+        {
+            public CompanyDataPoco CP { get; set; }
+            public double? Fitness { get; set; }
+        }
+    }
     }
 }
--- Recodme.Labs.MarketAnalyzer.BusinessLayer/BusinessObjects/AnalysisBusinessObject.cs
+++ Recodme.Labs.MarketAnalyzer.BusinessLayer/BusinessObjects/AnalysisBusinessObject.cs
@@ -1,13 +1,41 @@
-﻿using Recodme.Labs.MarketAnalyzer.DataAccessLayer;
+﻿using Recodme.Labs.MarketAnalyzer.Analysis;
+using Recodme.Labs.MarketAnalyzer.DataAccessLayer;
+using Recodme.Labs.MarketAnalyzer.DataLayer.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Recodme.Labs.MarketAnalyzer.BusinessLayer.BusinessObjects
 {
     public class AnalysisBusinessObject
     {
-        public void AnalyseStocks()
+        public List<Poco> GetStockFitness()
         {
             var dao = new CompanyDataAccessObject();
-            var result = dao.GetCompaniesInfo();
+            var companiesDataPoco = dao.GetCompaniesInfo();
+            var count = 0;
+            var list = new List<Poco>();
+            foreach (var companyDataPoco in companiesDataPoco)
+            {
+                var stockAnalysis = new StockAnalysis(companyDataPoco);
+                var stockFitness = new StockFitness(stockAnalysis);
+                var total = stockFitness.TotalFitness;
+                if (total != null)
+                {
+                    list.Add(new Poco { CP = companyDataPoco, Fitness = total });
+                    count++;
+                }
+            }
+            foreach (var item in list.OrderBy(l => l.Fitness))
+            {
+                Console.WriteLine(item.CP.Company.Ticker + " " + item.Fitness);
+            }
+            return list;
+        }
+        public class Poco
+        {
+            public CompanyDataPoco CP { get; set; }
+            public double? Fitness { get; set; }
         }
     }
 }
