cc11e8ea81e89eaa2930d20744c8258df583ee0c
s-cunha <s.cunha@recodme.pt>
2020-08-27 09:56:45 +0100

    Chnages to HomeController, HomeDataPoco, Index

--- Recodme.Labs.MarketAnalyzer.WebAPI/Models/Support/HomeDataPoco.cs
+++ Recodme.Labs.MarketAnalyzer.WebAPI/Models/Support/HomeDataPoco.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,18 @@ namespace Recodme.Labs.MarketAnalyzer.WebAPI.Models.Support
 {
     public class HomeDataPoco
     {
+
         public string Ticker { get; set; }
+
+        [Display(Name = "Company Name")]
         public string CompanyName { get; set; }
+
+        [Display(Name = "Market Analyzer Rank")]
         public int MarketAnalyzerRank { get; set; }
+
+        [Display(Name = "Forbes 2000 Rank")]
         public int? Forbes2000Rank { get; set; }
+
         public double? Fitness { get; set; }
     }
 }
