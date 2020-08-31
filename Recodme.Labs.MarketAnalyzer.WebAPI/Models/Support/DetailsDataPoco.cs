bb1d606a814626bfd58702c43e26f059b738cb2b
Analuizasilva <a.silva@recodme.pt>
2020-08-31 09:34:58 +0100

    Added charts Roic, Equity, Revenue

--- Recodme.Labs.MarketAnalyzer.WebAPI/Models/Support/DetailsDataPoco.cs
+++ Recodme.Labs.MarketAnalyzer.WebAPI/Models/Support/DetailsDataPoco.cs
@@ -1,6 +1,7 @@
 ﻿using Recodme.Labs.MarketAnalyzer.Analysis;
 using Recodme.Labs.MarketAnalyzer.Analysis.Support;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Recodme.Labs.MarketAnalyzer.WebAPI.Models.Support
 {
@@ -12,30 +13,52 @@ namespace Recodme.Labs.MarketAnalyzer.WebAPI.Models.Support
         public int MarketAnalyzerRank { get; set; }
         public decimal? StockPrice { get; set; }
 
-        public List<ExtractedValue> Marketcap{ get; set; }
+        public List<ExtractedValue> Marketcap { get; set; }
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public List<ExtractedValue> RevenueGrowth { get; set; }
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public List<ExtractedValue> EpsGrowth { get; set; }
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public List<ExtractedValue> EquityGrowth { get; set; }
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public List<ExtractedValue> EquityNominalValues { get; set; }
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public List<ExtractedValue> EPSNominalValues { get; set; }
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public List<ExtractedValue> RevenueNominalValues { get; set; }
 
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double? DebtToEquity { get; set; }
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double? AssetsToLiabilities { get; set; }
-        public double? Roic { get; set; }
-        public double? Equity{ get; set; }
+        [DisplayFormat(DataFormatString = "{0:F2}")]
+        public List<ExtractedValue> Roic { get; set; }
+        [DisplayFormat(DataFormatString = "{0:F2}")]
+        public double? Equity { get; set; }
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double? EPS { get; set; }
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double? Revenue { get; set; }
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double? PERatio { get; set; }
 
-
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double? DebtToEquityFitness { get; set; }
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double? AssetsToLiabilitiesFitness { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double? RoicFitness { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double? EquityFitness { get; set; }
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double? EPSFitness { get; set; }
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double? RevenueFitness { get; set; }
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double? PERatioFitness { get; set; }
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double? TotalFitness { get; set; }
 
         public double? WeightNumberRoic { get; set; }
