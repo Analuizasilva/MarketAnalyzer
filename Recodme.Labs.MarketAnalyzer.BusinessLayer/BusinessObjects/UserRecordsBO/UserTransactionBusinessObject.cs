c077ab705d43885aaf6a708bc1b3cac48c992c59
Marco Pereira <m.pereira@recodme.pt>
2020-09-04 17:22:02 +0100

    Changes to UserTransactions

--- Recodme.Labs.MarketAnalyzer.BusinessLayer/BusinessObjects/UserRecordsBO/UserTransactionBusinessObject.cs
+++ Recodme.Labs.MarketAnalyzer.BusinessLayer/BusinessObjects/UserRecordsBO/UserTransactionBusinessObject.cs
@@ -137,6 +137,71 @@ namespace Recodme.Labs.MarketAnalyzer.BusinessLayer.BusinessObjects.UserRecordsB
         }
         #endregion
 
+        #region Delete
+
+        //public virtual OperationResult Delete(UserTransaction userTransaction)
+        //{
+        //    try
+        //    {
+        //        using var transactionScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled);
+        //        _dao.Update(profile);
+        //        transactionScope.Complete();
+        //        return new OperationResult { Success = true };
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        return new OperationResult() { Success = false, Exception = e };
+        //    }
+        //}
+
+        //public async virtual Task<OperationResult> DeleteAsync(Profile profile)
+        //{
+        //    try
+        //    {
+        //        using var transactionScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled);
+        //        await _dao.DeleteAsync(profile);
+        //        transactionScope.Complete();
+        //        return new OperationResult { Success = true };
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        return new OperationResult() { Success = false, Exception = e };
+        //    }
+        //}
+
+        //public virtual OperationResult Delete(Guid id)
+        //{
+        //    try
+        //    {
+
+        //        using var transactionScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled);
+        //        _dao.Delete(id);
+        //        transactionScope.Complete();
+        //        return new OperationResult { Success = true };
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        return new OperationResult() { Success = false, Exception = e };
+        //    }
+        //}
+
+        //public async virtual Task<OperationResult> DeleteAsync(Guid id)
+        //{
+        //    try
+        //    {
+        //        using var transactionScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled);
+        //        await _dao.DeleteAsync(id);
+        //        transactionScope.Complete();
+        //        return new OperationResult { Success = true };
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        return new OperationResult() { Success = false, Exception = e };
+        //    }
+        //}
+
+        #endregion
+
         #region Update
         public OperationResult Update(UserTransaction userTransaction)
         {
--- Recodme.Labs.MarketAnalyzer.DataLayer/Base/Entity.cs
+++ Recodme.Labs.MarketAnalyzer.DataLayer/Base/Entity.cs
@@ -9,7 +9,21 @@ namespace Recodme.Labs.MarketAnalyzer.DataLayer.Base
         public Guid Id { get; private set; }
         public DateTime DateCreated { get; private set; }
         public DateTime DateUpdated { get; protected set; }
-    
+
+        private bool _isDeleted;
+        public bool IsDeleted
+        {
+            get
+            {
+                return _isDeleted;
+            }
+            set
+            {
+                _isDeleted = value;
+                RegisterChange();
+            }
+        }
+
 
         protected virtual void RegisterChange()
         {
@@ -20,7 +34,7 @@ namespace Recodme.Labs.MarketAnalyzer.DataLayer.Base
         {
             Id = Guid.NewGuid();
             DateUpdated = DateTime.UtcNow;
-            DateCreated = DateCreated;          
+            DateCreated = DateCreated;  
         }
 
         protected Entity(Guid id, DateTime createdAt, DateTime updatedAd)
@@ -28,6 +42,14 @@ namespace Recodme.Labs.MarketAnalyzer.DataLayer.Base
             Id = id;
             DateCreated = createdAt;
             DateUpdated = updatedAd;        
+        }
+
+        protected Entity(Guid id, DateTime createdAt, DateTime updatedAd, bool isDeleted)
+        {
+            Id = id;
+            DateCreated = createdAt;
+            DateUpdated = updatedAd;
+            IsDeleted = isDeleted;
         }
     }
 }
