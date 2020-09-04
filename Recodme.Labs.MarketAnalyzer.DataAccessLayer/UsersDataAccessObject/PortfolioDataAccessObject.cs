2a2d916e94980159031b00197103223c755061ad
Marco Pereira <m.pereira@recodme.pt>
2020-09-04 11:26:42 +0100

    changes to DAO and BO

--- Recodme.Labs.MarketAnalyzer.DataAccessLayer/UsersDataAccessObject/PortfolioDataAccessObject.cs
+++ Recodme.Labs.MarketAnalyzer.DataAccessLayer/UsersDataAccessObject/PortfolioDataAccessObject.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Recodme.Labs.MarketAnalyzer.DataAccessLayer
+namespace Recodme.Labs.MarketAnalyzer.DataAccessLayer.UsersDataAccessObject
 {
     public class PortfolioDataAccessObject
     {
