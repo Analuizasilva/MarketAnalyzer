8d586e2006b383eff28f620d1902c04a27abe5d5
s-cunha <s.cunha@recodme.pt>
2020-09-07 17:06:30 +0100

    Changes to totalsValues e TransactionAnalysis

--- Analyzer/Support/TotalsValues.cs
+++ Analyzer/Support/TotalsValues.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Recodme.Labs.MarketAnalyzer.DataLayer.UserRecords;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,10 +7,25 @@ namespace Recodme.Labs.MarketAnalyzer.Analysis.Support
 {
     public class TotalsValues
     {
-        public double? StocksOwned { get; set; }
-        public double? TotalInvested { get; set; }
-        public double? TotalWithdrawn { get; set; }
-        public double? TotalValue { get; set; }
+        public string CompanyName { get; set; }
+        public double? SharesOwned { get; set; }
+        public double? SharesBought { get; set; }
+        public double? SharesSold { get; set; }
+
+
+        public double? ShareValue { get; set; }
+
+        public double? TotalInvested { get; set; } //shares bought * share value
+
+
+        public double? TotalWithdrawn { get; set; } // shares sold * share value
+
+        public double? TotalValue { get; set; } // shares owned * share value
+
+        public double? TotalGainLoss { get; set; } //total withdrwan + total value - total invested
+
+        public double? Balance { get; set; } //total withdrawn + total value
+
 
 
     }
