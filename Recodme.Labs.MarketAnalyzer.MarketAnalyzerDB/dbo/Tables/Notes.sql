959b68c4b9b50b0d894072fc68acf668b769ed47
s-cunha <s.cunha@recodme.pt>
2020-09-07 15:10:41 +0100

    Added notes to dataLayer and db

---
+++ Recodme.Labs.MarketAnalyzer.MarketAnalyzerDB/dbo/Tables/Notes.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Notes] (
+    [Description]  NVARCHAR (MAX)   NULL,
+    [CompanyId]    UNIQUEIDENTIFIER NULL,
+    [Id]           UNIQUEIDENTIFIER NOT NULL,
+    [AspNetUserId] NVARCHAR (450)   NULL,
+    CONSTRAINT [PK_Notes] PRIMARY KEY CLUSTERED ([Id] ASC),
+    FOREIGN KEY ([AspNetUserId]) REFERENCES [dbo].[AspNetUsers] ([Id]),
+    FOREIGN KEY ([CompanyId]) REFERENCES [dbo].[Companies] ([Id])
+);
+
