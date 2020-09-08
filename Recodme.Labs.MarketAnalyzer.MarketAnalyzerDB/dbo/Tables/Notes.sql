322a058a2dd2183e92356f32ec842ea5ddbe8386
Marco Pereira <m.pereira@recodme.pt>
2020-09-08 11:52:10 +0100

    MarketAnalyzerDB updated

--- Recodme.Labs.MarketAnalyzer.MarketAnalyzerDB/dbo/Tables/Notes.sql
+++ Recodme.Labs.MarketAnalyzer.MarketAnalyzerDB/dbo/Tables/Notes.sql
@@ -1,10 +1,19 @@
 ﻿CREATE TABLE [dbo].[Notes] (
-    [Description]  NVARCHAR (MAX)   NULL,
-    [CompanyId]    UNIQUEIDENTIFIER NULL,
-    [Id]           UNIQUEIDENTIFIER NOT NULL,
-    [AspNetUserId] NVARCHAR (450)   NULL,
+    [Description]                     NVARCHAR (MAX)   NULL,
+    [CompanyId]                       UNIQUEIDENTIFIER NULL,
+    [Id]                              UNIQUEIDENTIFIER NOT NULL,
+    [AspNetUserId]                    NVARCHAR (450)   NULL,
+    [WeightNumberRoic]                FLOAT (53)       NULL,
+    [WeightNumberEquity]              FLOAT (53)       NULL,
+    [WeightNumberEPS]                 FLOAT (53)       NULL,
+    [WeightNumberRevenue]             FLOAT (53)       NULL,
+    [WeightNumberPERatio]             FLOAT (53)       NULL,
+    [WeightNumberDebtToEquity]        FLOAT (53)       NULL,
+    [WeightNumberAssetsToLiabilities] FLOAT (53)       NULL,
     CONSTRAINT [PK_Notes] PRIMARY KEY CLUSTERED ([Id] ASC),
-    FOREIGN KEY ([AspNetUserId]) REFERENCES [dbo].[AspNetUsers] ([Id]),
-    FOREIGN KEY ([CompanyId]) REFERENCES [dbo].[Companies] ([Id])
+    CONSTRAINT [FK__Notes__AspNetUse__5CD6CB2B] FOREIGN KEY ([AspNetUserId]) REFERENCES [dbo].[AspNetUsers] ([Id]),
+    CONSTRAINT [FK__Notes__CompanyId__5DCAEF64] FOREIGN KEY ([CompanyId]) REFERENCES [dbo].[Companies] ([Id])
 );
+
+
 
