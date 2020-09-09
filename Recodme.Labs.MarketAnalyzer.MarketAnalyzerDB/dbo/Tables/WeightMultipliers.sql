5b5292586fa4d5493b393bffad4cdebef627d1c8
Marco Pereira <m.pereira@recodme.pt>
2020-09-09 15:51:18 +0100

    MarketAnalyzerDB Updated

--- Recodme.Labs.MarketAnalyzer.MarketAnalyzerDB/dbo/Tables/WeightMultipliers.sql
+++ Recodme.Labs.MarketAnalyzer.MarketAnalyzerDB/dbo/Tables/WeightMultipliers.sql
@@ -1,16 +1,18 @@
 ﻿CREATE TABLE [dbo].[WeightMultipliers] (
-    [Id]                              NCHAR (10)     NOT NULL,
-    [AspNetUserId]                    NVARCHAR (450) NULL,
-    [WeightNumberRoic]                FLOAT (53)     NULL,
-    [WeightNumberEquity]              FLOAT (53)     NULL,
-    [WeightNumberEPS]                 FLOAT (53)     NULL,
-    [WeightNumberRevenue]             FLOAT (53)     NULL,
-    [WeightNumberPERatio]             FLOAT (53)     NULL,
-    [WeightNumberDebtToEquity]        FLOAT (53)     NULL,
-    [WeightNumberAssetsToLiabilities] FLOAT (53)     NULL,
-    [DateCreated]                     DATETIME2 (3)  NULL,
-    [DateUpdated]                     DATETIME2 (3)  NULL,
+    [Id]                              UNIQUEIDENTIFIER NOT NULL,
+    [AspNetUserId]                    NVARCHAR (450)   NULL,
+    [WeightNumberRoic]                FLOAT (53)       NULL,
+    [WeightNumberEquity]              FLOAT (53)       NULL,
+    [WeightNumberEPS]                 FLOAT (53)       NULL,
+    [WeightNumberRevenue]             FLOAT (53)       NULL,
+    [WeightNumberPERatio]             FLOAT (53)       NULL,
+    [WeightNumberDebtToEquity]        FLOAT (53)       NULL,
+    [WeightNumberAssetsToLiabilities] FLOAT (53)       NULL,
+    [DateCreated]                     DATETIME2 (3)    NULL,
+    [DateUpdated]                     DATETIME2 (3)    NULL,
     CONSTRAINT [PK_WeightMultipliers] PRIMARY KEY CLUSTERED ([Id] ASC),
-    FOREIGN KEY ([AspNetUserId]) REFERENCES [dbo].[AspNetUsers] ([Id])
+    CONSTRAINT [FK__WeightMul__AspNe__73BA3083] FOREIGN KEY ([AspNetUserId]) REFERENCES [dbo].[AspNetUsers] ([Id])
 );
+
+
 
