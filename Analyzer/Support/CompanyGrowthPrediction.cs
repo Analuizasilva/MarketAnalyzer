2c51724250023443859bdd1e4ea5a9fd0fbfd719
s-cunha <s.cunha@recodme.pt>
2020-09-25 11:39:24 +0100

    CompanyGrowthPrediction

---
+++ Analyzer/Support/CompanyGrowthPrediction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Recodme.Labs.MarketAnalyzer.Analysis.Support
+{
+    public class CompanyGrowthPrediction
+    {
+        public Guid CompanyId { get; set; }
+        public int Year { get; set; }
+        public double? MinValue { get; set; }
+        public double? EspectedValue { get; set; }
+        public double? MaxValue { get; set; }
+
+        public double? MinValueFiveYears { get; set; }
+        public double? EspectedValueFiveYears { get; set; }
+        public double? MaxValueFiveYears { get; set; }
+
+        public double? MinValueThreeYears { get; set; }
+        public double? EspectedValueThreeYears { get; set; }
+        public double? MaxValueThreeYears { get; set; }
+    }
+}
